f47b24b651b9f7941e556bbb9e2e67eb9d34fc47
unknown <pedrocorsini@outlook.com>
2025-08-18 20:50:39 -0300

    first commit

---
+++ lab02/src/main/java/br/unifei/ecot12/industria/Main.java
@@ -0,0 +1,20 @@
+package br.unifei.ecot12.industria;
+
+public class Main {
+    public static void main(String[] args) {
+        Departamento d = new Departamento();
+        d.setNome("IESTI");
+        Funcionario f = new Funcionario();
+        f.setNome("Corsini");
+        f.setDepartamento(d);
+        Projeto p = new Projeto();
+        p.setNumero(666);
+        p.setDepartamento(d);
+        p.setGerente(f);
+        Componente c = new Componente();
+        c.setProjeto(p);
+        p.getComponentes().add(c);
+        System.out.println(p.getGerente().getNome());
+
+    }
+}
