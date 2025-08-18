7db9516a0d3ed4825f97c150713e1aa34acf99b4
unknown <pedrocorsini@outlook.com>
2025-08-18 20:59:21 -0300

    First commit to labs branch

---
+++ lab02/src/main/java/br/unifei/ecot12/industria/Funcionario.java
@@ -0,0 +1,44 @@
+package br.unifei.ecot12.industria;
+
+import java.sql.Date;
+
+public class Funcionario {
+    private int matricula;
+    private String nome;
+    private Date admissao;
+    private float salario;
+    private Departamento departamento;
+    
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Date getAdmissao() {
+        return admissao;
+    }
+    public void setAdmissao(Date admissao) {
+        this.admissao = admissao;
+    }
+    public float getSalario() {
+        return salario;
+    }
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    
+}
