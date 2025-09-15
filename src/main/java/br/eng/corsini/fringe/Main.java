992d700f6a345c43f99048be63b99fc9a2f92cd3
Pedro <pedrocorsini@outlook.com>
2025-09-15 17:01:15 -0300

    adding main

--- src/main/java/br/eng/corsini/fringe/Main.java
+++ src/main/java/br/eng/corsini/fringe/Main.java
@@ -2,6 +2,14 @@ package br.eng.corsini.fringe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Real r = new Real();
+        Agente a = new Agente(r);
+        a.setNome("Olivia");
+        a.setIdade(26);
+        System.out.println(a.incapcita());
+        Cientista c = new Cientista(new Alternativo());
+        c.setNome("Peter");
+        c.setIdade(26);
+        System.out.println(c.incapcita());
     }
 }
